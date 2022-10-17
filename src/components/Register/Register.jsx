5409f139cdf4fce4855479509e536274fd56d130
ThunderGer <jaames11@hotmail.com>
2022-10-17 11:53:37 -0500

    delete comments

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -44,13 +44,7 @@ export const Register = () => {
                                                             <i className="fa fa-right-to-bracket orange"></i>
                                                             Siguiente
                                                         </button>
-                                                    </div>
-                                                    {/* <div className        ="text-center pt-1 mb-1">
-                                                        <p><a className       = "">Registrarse</a></p>
-                                                    </div>
-                                                    <div className            = "d-flex align-items-center justify-content-center">
-                                                        <a className          = "">¿Olvido su contraseña?</a>
-                                                    </div> */}
+                                                    </div>                                                    
                                                 </div>
                                             </div>
                                         </div>                                        
