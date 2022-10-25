f30656951b963f8e551b54653cfc8f3ab0862e0c
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-24 19:43:35 -0500

    Arreglo Componente Boton

--- src/components/Boton/Boton.jsx
+++ src/components/Boton/Boton.jsx
@@ -2,7 +2,8 @@ export const Boton = (props) =>{
     return(
         <>
         <button className="btn btn-light" type="button">
-            <i className="fa fa-right-to-bracket orange">{props.texto}</i>
+            <i className="fa fa-right-to-bracket orange"></i>
+            {props.texto}
         </button>
         </>
     )
